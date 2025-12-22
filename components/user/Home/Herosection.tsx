7cd182ba4d78e32db6d279d54aa8768544cbe8ce
lovaraju431902 <lovarajuk902431@gmail.com>
2025-12-22 07:08:17 +0530

    push the code remotely for first time

---
+++ components/user/Home/Herosection.tsx
@@ -0,0 +1,29 @@
+
+const Herosection = () => {
+  return (
+    <>
+    {/* <div className="relative">
+     <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-indigo-500 rounded-full blur-3xl animate-pulse" >
+      <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-purple-500 rounded-full blur-3xl animate-pulse" style={{ animationDelay: '1s' }} >
+      <div className="absolute top-1/2 right-1/3 w-64 h-64 bg-cyan-500 rounded-full blur-3xl animate-pulse" style={{ animationDelay: '0.5s' }} >
+      </div>
+      </div>
+      </div>
+      </div> */}
+         
+
+
+         <div className="bg-linear-to-r h-screen from-slate-100 to-purple-200">
+
+      
+         </div>
+
+
+
+
+     
+      </>
+  )
+}
+
+export default Herosection
